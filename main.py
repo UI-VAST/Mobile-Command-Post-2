9c5b68dfbe9145ad704cbffbc48da574a594a014
gmngmstr <shaunmartin1998@yahoo.com>
2022-04-07 18:20:35 -0700

    fixed tuple and ring test

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from adafruit_rockblock import RockBlock
 
 import serial
 
-debug = False
+debug = True
 tsym = 'F'
 port = "/dev/serial0"
 baud = 19200
 logger = Logger("RockBlock", debug)
 # uart = serial.Serial("/dev/serial0", 19200)
-uart = serial.Serial(port)
+uart = serial.Serial(port, baud)
 # uart = board.UART()
 # uart.baudrate = 19200
 
@@ -34,24 +34,24 @@ retry = 0
 counter = 0
 while 1:
     # set the text
-
-    if counter % 10 == 0:  # Send every
-        # try a satellite Short Burst Data transfer
-        status = rb.satellite_transfer()
-        logger.log("Talking to satellite... ", str(status))  # loop as needed
-
-    if status[0] > 8 and counter % 10 == 0:
-        status = rb.satellite_transfer()
-        logger.log(str(retry), str(status))
-        data = rb.text_in
-        logger.log("Signal Quality: ", str(rb.signal_quality))
-        logger.log("Setting Text... ", data)
-        retry += 1
-
-    if 8 >= status[0] > -1:
-        logger.log("Received!")
-        status[0] = -1
-        retry = 0
+    print(rb.ring_indication)
+    # if counter % 10 == 0 and status[0] == -1:
+    #     # try a satellite Short Burst Data transfer
+    #     status = rb.satellite_transfer()
+    #     logger.log("Talking to satellite... ", str(status))  # loop as needed
+    #
+    # if status[0] > 8 and counter % 10 == 0:
+    #     status = rb.satellite_transfer()
+    #     logger.log(str(retry), str(status))
+    #     data = rb.text_in
+    #     logger.log("Signal Quality: ", str(rb.signal_quality))
+    #     logger.log("Setting Text... ", data)
+    #     retry += 1
+    #
+    # if 8 >= status[0] > -1:
+    #     logger.log("Received!")
+    #     status = (-1, 0, 0, 0, 0, 0)
+    #     retry = 0
 
     time.sleep(1)  # Sleep for 1 second
 
